269fce77cdd0eb9c9f14de955c15c70808b38c44
Fhellipy Conceição <ffhellipyc.santana@gmail.com>
2023-01-30 18:30:47 -0300

    feat: add anime description card  content

--- src/components/common/Description/index.tsx
+++ src/components/common/Description/index.tsx
@@ -1,66 +1,81 @@
-import { Anime, AnimeResponseDTO } from "@app/dto/anime";
+import { getAnime } from "@services/animes";
 import Image from "next/image";
 import css from "./styles.module.css";
 
-async function Description() {
-  const animes = await getDescriptionAnime();
+interface Props {
+  idAnime: string;
+}
 
-  return (
-    <>
+async function Description({ idAnime }: Props) {
+  const anime = await getAnime(idAnime);
+
+  if (anime) {
+    return (
       <div className={css.container}>
-        {animes.map((anime) => (
-          <div className={css.wrapper} key={anime.id}>
-            <div className={css.frontCover}>
-              <Image
-                src={anime.images.tiny}
-                alt={anime.title}
-                width={225}
-                height={318}
-              />
-            </div>
+        <div className={css.titles}>
+          <h1>{anime.title}</h1>
+          <h3>{anime.title_japanese}</h3>
+        </div>
 
-            <div className={css.description}></div>
+        <div className={css.wrapper}>
+          <div>
+            <Image
+              src={anime.images.tiny}
+              alt={anime.title}
+              width={225}
+              height={318}
+            />
           </div>
-        ))}
-      </div>
 
-      <pre>{JSON.stringify(animes, null, 2)}</pre>
-    </>
-  );
+          <div className={css.description}>
+            <ul className={css.items}>
+              <span>
+                <li>
+                  <strong>Favoritado por:</strong> {anime.favorites} pessoas
+                </li>
+
+                <li>
+                  <strong>Duração:</strong> {anime.duration}
+                </li>
+                <li>
+                  <strong>Ano:</strong> {anime.year}
+                </li>
+                <li>
+                  <strong>{anime.episodes} </strong> episódio(s)
+                </li>
+              </span>
+
+              <p className={css.synopsis}>{anime.synopsis}</p>
+            </ul>
+          </div>
+        </div>
+
+        <div className={css.medias}>
+          {anime.trailer?.url ? (
+            <iframe
+              width="560"
+              height="315"
+              src={anime.trailer.embed_url}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+          ) : (
+            ""
+          )}
+
+          <span>
+            <h2>Títulos Relacionados</h2>
+
+            {anime.title_synonyms?.map((title, index) => {
+              return <p key={index}>{title}</p>;
+            })}
+          </span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export { Description };
-
-const API_URL = process.env.NEXT_PUBLIC_API;
-
-function isAnimeResponseDTO(
-  data: unknown
-): data is { data: AnimeResponseDTO[] } {
-  if (!data) return false;
-  if (typeof data !== "object") return false;
-  return "data" in data;
-}
-
-async function getDescriptionAnime(): Promise<Anime[]> {
-  const response = await fetch(`${API_URL}/top/anime/`);
-
-  const json = await response.json();
-
-  const result = isAnimeResponseDTO(json) ? json : { data: [] };
-
-  return result.data.map((anime) => ({
-    id: anime.mal_id,
-    title: anime.title,
-    title_japanese: anime.title_japanese,
-    duration: anime?.duration,
-    favorites: anime.favorites,
-    year: anime.year,
-
-    images: {
-      large: anime.images.webp.large_image_url,
-      tiny: anime.images.webp.image_url,
-    },
-    episodes: anime.episodes,
-    synopsis: anime.synopsis,
-  }));
-}
