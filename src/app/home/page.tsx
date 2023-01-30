58b382c6a9ca16b9fa409dd3f672ce1be7ab696d
Fhellipy Conceição <ffhellipyc.santana@gmail.com>
2023-01-30 18:29:13 -0300

    feat: add homepage content

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,46 @@
-import { Description } from "@components/common/Description";
+import { getTopAnimes } from "@services/animes";
+import Image from "next/image";
+import Link from "next/link";
+import css from "./styles.module.css";
+import { Inter } from "@next/font/google";
 
-export default async function Home() {
+const inter = Inter({ subsets: ["latin"] });
+
+export default async function HomePage() {
+  const animes = await getTopAnimes();
+
+  const formatTitle = (title: string) => {
+    if (title.length >= 15) {
+      return title.substr(0, 15) + "...";
+    } else {
+      return title;
+    }
+  };
+
   return (
-    <main>
-      <h1>Home</h1>
-      <Description />
+    <main className={css.main}>
+      <h1>Top Animes</h1>
+      <div className={css.container}>
+        {animes.map((anime) => (
+          <Link
+            href={`/anime/${anime.id}`}
+            className={css.wrapper}
+            key={anime.id}
+          >
+            <Image
+              src={anime.images.tiny}
+              alt={anime.title}
+              width={225}
+              height={318}
+            />
+
+            <ul className={css.titles}>
+              <li>{formatTitle(anime.title)}</li>
+              <li>{formatTitle(anime.title_japanese)}</li>
+            </ul>
+          </Link>
+        ))}
+      </div>
     </main>
   );
 }
