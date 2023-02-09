8b30a428a0681baf9c07ac868b1da5b80a97a272
Fhellipy Conceição <ffhellipyc.santana@gmail.com>
2023-02-09 18:42:06 -0300

    feat: add logged in user validation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import SideBar from "@components/templates/SideBar";
 import { Inter } from "@next/font/google";
 import "../styles/globals.css";
 import css from "./styles.module.css";
+import { cookies } from "next/headers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,17 +11,36 @@ interface Props {
   children: React.ReactNode;
 }
 
+const getAuthStatus = () => {
+  const nextCookies = cookies(); // Get cookies object
+  const cookie = nextCookies.get("is-logged-in"); // Find cookie
+
+  const isLoggedIn = cookie?.value === "true";
+
+  return {
+    isLoggedIn,
+  };
+};
+
 export default function RootLayout({ children }: Props) {
+  const { isLoggedIn } = getAuthStatus();
+
   return (
     <html lang="pt-BR">
       <head />
       <body className={css.body}>
-        <Header />
+        {isLoggedIn ? (
+          <>
+            <Header />
 
-        <div className={css.container}>
-          <SideBar />
+            <div className={css.container}>
+              <SideBar />
+              <div className={css.content}>{children}</div>
+            </div>
+          </>
+        ) : (
           <div className={css.content}>{children}</div>
-        </div>
+        )}
       </body>
     </html>
   );
