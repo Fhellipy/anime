84cf342e516e6cebc93f12973ad8d037e428f87d
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2023-02-10 16:40:35 -0300

    feat: add lib react-hook-form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,50 +2,56 @@
 import { useState } from "react";
 import css from "./styles.module.css";
 import { Inter } from "@next/font/google";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { User } from "@dto/user";
+import { InputForm } from "@components/ui/InputForm";
+import { validatePassword } from "./validators/password";
+import { validateUsername } from "./validators/username";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function LoginPage() {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<User>();
+
+  const onSubmit: SubmitHandler<User> = (data) => {
+    console.log("data", data);
+  };
 
   return (
     <main className={css.main}>
       <header>Logo</header>
 
-      <div className={css.container}>
+      <form className={css.container} onSubmit={handleSubmit(onSubmit)}>
         <div className={css.content}>
-          <span className={css.wrapper}>
-            <label htmlFor="username">Usuário: </label>
-            <input
-              type="text"
-              name="username"
-              id="username"
-              placeholder="Usuário"
-              onChange={(ev) => setUserName(ev.target.value)}
-            />
-          </span>
+          <InputForm
+            error={errors.username?.message}
+            validator={register("username", validateUsername)}
+            placeholder="Usuário"
+            label="Usuário"
+            type="text"
+          />
 
-          <span className={css.wrapper}>
-            <label htmlFor="password">Senha: </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="Senha"
-              onChange={(ev) => setPassword(ev.target.value)}
-            />
-          </span>
+          <InputForm
+            error={errors.password?.message}
+            validator={register("password", validatePassword)}
+            placeholder="*********"
+            label="Senha"
+            type="password"
+          />
         </div>
 
         <button type="submit" className={css.buttonSignIn}>
           Login
         </button>
-      </div>
+      </form>
 
       <span className={css.signUp}>
         <p>Ainda não tem uma conta?</p>
-        <a>Cadastrar-se</a>
+        <a href="/cadastrar">Cadastrar-se</a>
       </span>
     </main>
   );
