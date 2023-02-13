a07fb46d06f3d9bee7eb118fd0a0ab9c9c87003b
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2023-02-13 18:49:00 -0300

    refactor: change email error message

--- frontend/src/app/login/validators/email.ts
+++ frontend/src/app/login/validators/email.ts
@@ -3,6 +3,6 @@ import { RegisterOptions } from "react-hook-form";
 export const validateEmail: RegisterOptions = {
   required: {
     value: true,
-    message: "O email é obrigatório!",
+    message: "O email do usuário é obrigatório!",
   },
 };
