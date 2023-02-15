6b3bd5acb7f48b2e18a2bab5ba1513952464a940
Fhellipy Conceição <ffhellipyc.santana@gmail.com>
2023-02-15 18:27:31 -0300

    refactor: change pages generic layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,47 +1,56 @@
-import { Header } from "@components/templates/Header";
-import SideBar from "@components/templates/SideBar";
-import { Inter } from "@next/font/google";
-import "../styles/globals.css";
-import css from "./styles.module.css";
-import { cookies } from "next/headers";
+'use client';
+import { Header } from '@components/templates/Header';
+import SideBar from '@components/templates/SideBar';
+import { Inter } from '@next/font/google';
+import '../styles/globals.css';
+import css from './styles.module.css';
+import { cookies } from 'next/headers';
+import { QueryClientProvider } from 'react-query';
+import { queryClient } from '@config/queryClient';
+import { render } from 'react-dom';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 interface Props {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
-const getAuthStatus = () => {
-  const nextCookies = cookies(); // Get cookies object
-  const cookie = nextCookies.get("is-logged-in"); // Find cookie
+// const getAuthStatus = () => {
+// 	const nextCookies = cookies(); // Get cookies object
+// 	const cookie = nextCookies.get('is-logged-in'); // Find cookie
 
-  const isLoggedIn = cookie?.value === "true";
+// 	const isLoggedIn = cookie?.value === 'true';
 
-  return {
-    isLoggedIn,
-  };
-};
+// 	return {
+// 		isLoggedIn,
+// 	};
+// };
 
 export default function RootLayout({ children }: Props) {
-  const { isLoggedIn } = getAuthStatus();
+	// const { isLoggedIn } = getAuthStatus();
+	const isLoggedIn = false;
 
-  return (
-    <html lang="pt-BR">
-      <head />
-      <body className={css.body}>
-        {isLoggedIn ? (
-          <>
-            <Header />
+	const html = (
+		<QueryClientProvider client={queryClient}>
+			{isLoggedIn ? (
+				<>
+					<Header />
 
-            <div className={css.container}>
-              <SideBar />
-              <div className={css.content}>{children}</div>
-            </div>
-          </>
-        ) : (
-          <div className={css.contentLogin}>{children}</div>
-        )}
-      </body>
-    </html>
-  );
+					<div className={css.container}>
+						<SideBar />
+						<div className={css.content}>{children}</div>
+					</div>
+				</>
+			) : (
+				<div className={css.contentLogin}>{children}</div>
+			)}
+		</QueryClientProvider>
+	);
+
+	return (
+		<html lang="pt-BR">
+			<head />
+			<body className={css.body}>{html}</body>
+		</html>
+	);
 }
