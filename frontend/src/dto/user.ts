499d8f9bc860da94bc0b8ded78e3191bcc9d8fd4
Fhellipy Conceição <ffhellipyc.santana@gmail.com>
2023-02-24 18:42:39 -0300

    feat: add user logged interface

--- frontend/src/dto/user.ts
+++ frontend/src/dto/user.ts
@@ -1,7 +1,16 @@
 export interface User {
-  id: string;
-  username: string;
-  email: string;
-  password: string;
-  confirm_password?: string;
+	id: string;
+	username: string;
+	email: string;
+	password: string;
+	confirm_password?: string;
+}
+
+export interface UserLogged {
+	token: string;
+	user: {
+		id: string;
+		username: string;
+		email: string;
+	};
 }
