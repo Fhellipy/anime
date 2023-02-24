255cdeed5afc82bed2ae7d942f2a5a113ad28765
Fhellipy Conceição <ffhellipyc.santana@gmail.com>
2023-02-24 18:35:24 -0300

    feat: add cors and cookies parser settings

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,11 +3,16 @@ import { errorMiddleware } from "@middlewares/error";
 import Routes from "@routes";
 import bodyParser from "body-parser";
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { corsConfig } from "@config/corsConfig";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors(corsConfig));
+app.use(cookieParser());
 app.use("/", Routes);
 app.use(errorMiddleware);
 
