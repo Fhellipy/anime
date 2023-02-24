936d0fb14f6b15c8807285f02517c5b497643751
Fhellipy Conceição <ffhellipyc.santana@gmail.com>
2023-02-24 18:38:34 -0300

    feat: add session provider

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,50 +1,43 @@
-'use client';
-import { Header } from '@components/templates/Header';
-import SideBar from '@components/templates/SideBar';
-import { Inter } from '@next/font/google';
-import '../styles/globals.css';
-import css from './styles.module.css';
-import { cookies } from 'next/headers';
-import { QueryClientProvider } from 'react-query';
-import { queryClient } from '@config/queryClient';
-import { render } from 'react-dom';
+"use client";
+import { Header } from "@components/templates/Header";
+import SideBar from "@components/templates/SideBar";
+import { TOKEN_KEY } from "@config/env/api";
+import { queryClient } from "@config/queryClient";
+import { Inter } from "@next/font/google";
+import { SessionProvider } from "next-auth/react";
+import { Toaster } from "react-hot-toast";
+import { QueryClientProvider } from "react-query";
+import { useLocalStorage } from "src/hooks/useLocalStorage";
+import "../styles/globals.css";
+import css from "./styles.module.css";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 interface Props {
 	children: React.ReactNode;
 }
 
-// const getAuthStatus = () => {
-// 	const nextCookies = cookies(); // Get cookies object
-// 	const cookie = nextCookies.get('is-logged-in'); // Find cookie
-
-// 	const isLoggedIn = cookie?.value === 'true';
-
-// 	return {
-// 		isLoggedIn,
-// 	};
-// };
-
 export default function RootLayout({ children }: Props) {
-	// const { isLoggedIn } = getAuthStatus();
-	const isLoggedIn = false;
+	const [token, _] = useLocalStorage(TOKEN_KEY, "");
 
 	const html = (
-		<QueryClientProvider client={queryClient}>
-			{isLoggedIn ? (
-				<>
-					<Header />
+		<SessionProvider>
+			<QueryClientProvider client={queryClient}>
+				<Toaster />
+				{token ? (
+					<>
+						<Header />
 
-					<div className={css.container}>
-						<SideBar />
-						<div className={css.content}>{children}</div>
-					</div>
-				</>
-			) : (
-				<div className={css.contentLogin}>{children}</div>
-			)}
-		</QueryClientProvider>
+						<div className={css.container}>
+							<SideBar />
+							<div className={css.content}>{children}</div>
+						</div>
+					</>
+				) : (
+					<div className={css.contentLogin}>{children}</div>
+				)}
+			</QueryClientProvider>
+		</SessionProvider>
 	);
 
 	return (
