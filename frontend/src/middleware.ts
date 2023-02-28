3b024ccd0ab9eb5d48703e6eec7535c2f371e2d6
Fhellipy Conceição <ffhellipyc.santana@gmail.com>
2023-02-28 18:40:14 -0300

    feat: add validation middleware  that checks if the logged in user exists

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,15 +1,47 @@
-import { TOKEN_KEY } from "@config/env/api";
-import { getToken } from "next-auth/jwt";
+"use client";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-export async function middleware(req: NextRequest) {
-	const session = await getToken({ req, secret: TOKEN_KEY });
+type Profile = {
+	id: string;
+	username: string;
+	email: string;
+};
 
-	if (!session) {
+function isProfile(data: unknown): data is Profile {
+	if (typeof data !== "object") return false;
+	if (data === null) return false;
+	if (!data.hasOwnProperty("id")) return false;
+	return true;
+}
+
+export async function middleware(req: NextRequest) {
+	const token = req.cookies.get("token")?.value;
+
+	if (!token) {
 		const url = req.nextUrl.clone();
 		url.pathname = "/login";
 
+		return NextResponse.redirect(url);
+	}
+
+	try {
+		const response = await fetch(
+			process.env.NEXT_PUBLIC_BASE_API_URL + "/auth/profile",
+			{
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			}
+		);
+
+		const json = await response.json();
+
+		if (!isProfile(json)) throw new Error("Invalid profile");
+	} catch (error) {
+		console.log(error);
+		const url = req.nextUrl.clone();
+		url.pathname = "/login";
 		return NextResponse.redirect(url);
 	}
 
@@ -17,5 +49,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-	matcher: ["/home", "/configuration"],
+	matcher: ["/home", "/configuration", "/anime"],
 };
