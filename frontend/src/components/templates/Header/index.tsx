d7f442cedc26023efc55d1e7412480507597449a
Fhellipy Conceição <ffhellipyc.santana@gmail.com>
2023-02-28 18:46:27 -0300

    feta: add logout function

--- frontend/src/components/templates/Header/index.tsx
+++ frontend/src/components/templates/Header/index.tsx
@@ -1,17 +1,38 @@
+"use client";
+import { useUserContext } from "@context/user";
 import { Inter } from "@next/font/google";
+import Cookies from "js-cookie";
 import { FaUser } from "react-icons/fa";
 import css from "./styles.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 function Header() {
-  return (
-    <header className={css.header}>
-      <FaUser className={css.icon} />
+	const [user] = useUserContext();
 
-      <p className={css.text}>Fhellipy</p>
-    </header>
-  );
+	function logout() {
+		Cookies.remove("token");
+		localStorage.removeItem("token");
+		window.location.pathname = "/login";
+	}
+
+	return (
+		<header className={css.header}>
+			<FaUser className={css.icon} />
+
+			<p className={css.text}>
+				Olá, <strong>{user ? user.username : ""}</strong>
+			</p>
+			<button
+				className={css.button}
+				onClick={() => {
+					logout();
+				}}
+			>
+				Sair
+			</button>
+		</header>
+	);
 }
 
 export { Header };
