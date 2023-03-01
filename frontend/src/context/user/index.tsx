1e505f8f9721d8d23cd81305e93bf5074ef8804c
Fhellipy Conceição <ffhellipyc.santana@gmail.com>
2023-03-01 15:13:18 -0300

    refactor: change user context. Added functions signIn and signOut

--- frontend/src/context/user/index.tsx
+++ frontend/src/context/user/index.tsx
@@ -1,58 +1,71 @@
 "use client";
-import { StateType } from "@commonTypes/state";
-import {
-  createContext,
-  ReactNode,
-  useContext,
-  useEffect,
-  useState
-} from "react";
+import { useLocalStorage } from "@hooks/useLocalStorage";
+import { createContext, ReactNode, useContext, useState } from "react";
 
 type UserProviderProps = {
 	children: ReactNode;
 };
-type TypeUSer = {
+
+type TypeUser = {
 	id: string;
 	username: string;
 	email: string;
 };
 
-const UserContext = createContext<StateType<TypeUSer>>([
-	{} as TypeUSer,
-	() => {
-		return;
-	},
-]);
+type UserContextType = {
+	user: TypeUser | undefined;
+	signIn: (user: TypeUser) => void;
+	signOut: () => void;
+};
+
+const UserContext = createContext<UserContextType>({} as UserContextType);
+
+function isValidUser(data: unknown): data is TypeUser {
+	if (!data) return false;
+	if (typeof data !== "object") return false;
+
+	return "email" in data;
+}
 
 const AUTH_LS_KEY = "auth";
 
 export function UserProvider({ children }: UserProviderProps) {
-	const recoveredUser = JSON.parse(localStorage.getItem(AUTH_LS_KEY) ?? "{}");
+	const [authLsKey, setAuthLsKey] = useLocalStorage(
+		AUTH_LS_KEY,
+		JSON.stringify({})
+	);
 
-	const initialState = Object.keys(recoveredUser).length
-		? recoveredUser
-		: undefined;
+	const recoveredUser = authLsKey;
 
-	const [data, setUser] = useState(initialState);
+	const initialState = isValidUser(recoveredUser) ? recoveredUser : undefined;
 
-	useEffect(() => {
-		if (data && Object.keys(data).length) {
-			const { token: _, ...user } = data;
+	const [user, setUser] = useState<TypeUser | undefined>(initialState);
 
-			setUser(user.user);
-			localStorage.setItem(AUTH_LS_KEY, JSON.stringify(user));
-		}
-	}, [data]);
+	const signIn = (user: TypeUser) => {
+		setUser(user);
+		setAuthLsKey(JSON.stringify(user));
+	};
+
+	const signOut = () => {
+		setUser(undefined);
+		localStorage.removeItem(AUTH_LS_KEY);
+	};
 
 	return (
-		<UserContext.Provider value={[data, setUser]}>
+		<UserContext.Provider value={{ user, signIn, signOut }}>
 			{children}
 		</UserContext.Provider>
 	);
 }
 
 function useUserContext() {
-	return useContext(UserContext);
+	const context = useContext(UserContext);
+
+	if (!context) {
+		throw new Error("useUserContext needs to be used inside UserProvider");
+	}
+
+	return context;
 }
 
 export default UserProvider;
