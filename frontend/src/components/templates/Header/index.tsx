f4e176698c5156d0ea195ae576c8adb9afce8198
Fhellipy Conceição <ffhellipyc.santana@gmail.com>
2023-03-01 15:18:00 -0300

    feat: add user information

--- frontend/src/components/templates/Header/index.tsx
+++ frontend/src/components/templates/Header/index.tsx
@@ -2,26 +2,34 @@
 import { useUserContext } from "@context/user";
 import { Inter } from "@next/font/google";
 import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
 import { FaUser } from "react-icons/fa";
 import css from "./styles.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 function Header() {
-	const [user] = useUserContext();
+	const { user, signOut } = useUserContext();
+	const [userName, setUserName] = useState("");
 
 	function logout() {
 		Cookies.remove("token");
 		localStorage.removeItem("token");
 		window.location.pathname = "/login";
+
+		signOut();
 	}
+
+	useEffect(() => {
+		if (user) setUserName(user.username);
+	}, [user]);
 
 	return (
 		<header className={css.header}>
 			<FaUser className={css.icon} />
 
 			<p className={css.text}>
-				Olá, <strong>{user ? user.username : ""}</strong>
+				Olá, <strong>{userName}</strong>
 			</p>
 			<button
 				className={css.button}
