8905a265fe1a28a925cc9203ef25e86a86769e64
Fhellipy Conceição <ffhellipyc.santana@gmail.com>
2023-03-03 18:43:11 -0300

    feat: add anime  comments interface

--- frontend/src/dto/anime.ts
+++ frontend/src/dto/anime.ts
@@ -1,43 +1,61 @@
 export interface AnimeResponseDTO {
-  mal_id: number;
-  title: string;
-  title_japanese: string;
-  duration?: string | null;
-  favorites?: number | null;
-  year?: number | null;
-  images: {
-    webp: {
-      image_url: string;
-      large_image_url: string;
-    };
-  };
-  episodes: number;
-  synopsis?: string | null;
-  title_synonyms?: string[];
-  trailer?: {
-    youtube_id: string;
-    url: string;
-    embed_url: string;
-  };
+	mal_id: number;
+	title: string;
+	title_japanese: string;
+	duration?: string | null;
+	favorites?: number | null;
+	year?: number | null;
+	images: {
+		webp: {
+			image_url: string;
+			large_image_url: string;
+		};
+	};
+	episodes: number;
+	synopsis?: string | null;
+	title_synonyms?: string[];
+	trailer?: {
+		youtube_id: string;
+		url: string;
+		embed_url: string;
+	};
 }
 
 export interface Anime {
-  id: number;
-  title: string;
-  title_japanese: string;
-  duration?: string | null;
-  favorites?: number | null;
-  year?: number | null;
-  images: {
-    large: string;
-    tiny: string;
-  };
-  episodes: number;
-  synopsis?: string | null;
-  title_synonyms?: string[];
-  trailer?: {
-    youtube_id: string;
-    url: string;
-    embed_url: string;
-  };
+	id: number;
+	title: string;
+	title_japanese: string;
+	duration?: string | null;
+	favorites?: number | null;
+	year?: number | null;
+	images: {
+		large: string;
+		tiny: string;
+	};
+	episodes: number;
+	synopsis?: string | null;
+	title_synonyms?: string[];
+	trailer?: {
+		youtube_id: string;
+		url: string;
+		embed_url: string;
+	};
+}
+
+export interface AnimeComment {
+	idAnime: string;
+	comment?: string;
+}
+
+export interface AnimeCommentResponse {
+	id: string;
+	idAnime: string;
+	comment: string;
+	username: string;
+	userId: string;
+	createdAt: Date;
+}
+
+export interface DataAnime {
+	comments: AnimeCommentResponse[];
 }
