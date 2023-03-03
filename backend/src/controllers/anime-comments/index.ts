3d056d52a44d0c9c307a309b791724136a8d7863
Fhellipy Conceição <ffhellipyc.santana@gmail.com>
2023-03-03 18:41:40 -0300

    feat: add route to register and fetch comments from anime

---
+++ backend/src/controllers/anime-comments/index.ts
@@ -0,0 +1,48 @@
+import prisma from "@database";
+import { Anime as AnimeType } from "@dto/anime";
+import { authMiddleware } from "@middlewares/authMiddleware";
+import { BadRequestError } from "@utils/apiError";
+import { Request, Response, Router } from "express";
+
+const Anime = Router();
+
+Anime.post("/new", authMiddleware, async (req: Request, resp: Response) => {
+	const { idAnime, comment } = req.body as AnimeType;
+
+	const user = req.user;
+
+	if (!idAnime || !comment || !user) {
+		throw new BadRequestError("Falha ao cadastrar comentário!");
+	}
+
+	await prisma.animeComments.create({
+		data: {
+			idAnime,
+			comment,
+			userId: user.id,
+			createdAt: new Date(),
+			username: user.username,
+		},
+	});
+
+	return resp.status(201).send({
+		success: true,
+		message: "Comentário cadastrado com sucesso!",
+	});
+});
+
+Anime.get("/", authMiddleware, async (req: Request, resp: Response) => {
+	const { idAnime } = req.query;
+
+	if (typeof idAnime === "string") {
+		const comments = await prisma.animeComments.findMany({
+			where: { idAnime },
+		});
+
+		return resp.status(200).send({
+			comments,
+		});
+	}
+});
+
+export default Anime;
