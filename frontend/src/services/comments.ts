e32001118f92018903108de547574a14e19ff9ba
Fhellipy Conceição <ffhellipyc.santana@gmail.com>
2023-03-06 15:51:23 -0300

    feat: add useMutate from delete comment

--- frontend/src/services/comments.ts
+++ frontend/src/services/comments.ts
@@ -1,8 +1,13 @@
 "use client";
 import { toast } from "@components/ui/toast";
+import { queryClient } from "@config/queryClient";
 import { AnimeComment, AnimeCommentResponse } from "@dto/anime";
 import { ApiError, fetchApi } from "@lib/api";
 import { useMutation } from "react-query";
+
+type Anime = {
+	id: string;
+};
 
 function useMutateCommentRegister() {
 	return useMutation<AnimeCommentResponse, ApiError | Error, AnimeComment>(
@@ -18,6 +23,7 @@ function useMutateCommentRegister() {
 		{
 			onSuccess: () => {
 				toast.success("Comentário cadastrado com sucesso!");
+				queryClient.invalidateQueries("anime-comments");
 			},
 			onError(error) {
 				setTimeout(() => {
@@ -28,4 +34,29 @@ function useMutateCommentRegister() {
 	);
 }
 
-export { useMutateCommentRegister };
+function useMutateCommentDelete() {
+	return useMutation<Anime, ApiError | Error, Anime>(
+		"/anime-comments",
+		async ({ id }) => {
+			const response = await fetchApi(`/anime-comments`, {
+				method: "DELETE",
+				body: JSON.stringify({ id }),
+			});
+
+			return response.json();
+		},
+		{
+			onSuccess: () => {
+				toast.success("Comentário deletado com sucesso!");
+				queryClient.invalidateQueries("anime-comments");
+			},
+			onError(error) {
+				setTimeout(() => {
+					toast.error(error.message);
+				}, 500);
+			},
+		}
+	);
+}
+
+export { useMutateCommentRegister, useMutateCommentDelete };
