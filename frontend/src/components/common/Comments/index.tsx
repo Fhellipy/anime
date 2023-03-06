2035c5ee07f94464303e4946e43cd13a03b9059b
Fhellipy Conceição <ffhellipyc.santana@gmail.com>
2023-03-06 15:49:41 -0300

    reactor: separates the NewComment and CommentsAnime components

--- frontend/src/components/common/Comments/index.tsx
+++ frontend/src/components/common/Comments/index.tsx
@@ -1,9 +1,11 @@
 "use client";
+import { useUserContext } from "@context/user";
 import { AnimeCommentResponse, DataAnime } from "@dto/anime";
-import { useMutateCommentRegister } from "@services/comments";
+import { useMutateCommentDelete } from "@services/comments";
 import { useEffect, useState } from "react";
-import { useQuery } from "react-query";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { useQuery } from "react-query";
 import css from "./styles.module.css";
 
 type Props = {
@@ -11,25 +13,16 @@ type Props = {
 };
 
 function CommentsAnime({ idAnime }: Props) {
+	const { user } = useUserContext();
 	const [isCollapsed, setIsCollapsed] = useState(false);
-	const [newComment, setNewComment] = useState("");
-	const [comments, setComments] = useState<AnimeCommentResponse[]>();
+	const [comments, setComments] = useState<AnimeCommentResponse[]>([]);
 
-	const commentMutate = useMutateCommentRegister();
+	const commentMutate = useMutateCommentDelete();
 
 	const commentsData = useQuery<DataAnime, Error>([
 		"anime-comments",
 		{ idAnime },
 	]);
-
-	const registerComment = () => {
-		const data = {
-			idAnime,
-			comment: newComment,
-		};
-
-		commentMutate.mutate(data);
-	};
 
 	useEffect(() => {
 		if (commentsData.isSuccess) {
@@ -38,67 +31,66 @@ function CommentsAnime({ idAnime }: Props) {
 	}, [commentsData]);
 
 	return (
-		<section className={css.comments}>
-			<div className={css.newComment}>
-				<label htmlFor="comment">Novo comentário: </label>
-				<textarea
-					name="comment"
-					id="comment"
-					placeholder="Digite seu comentário..."
-					cols={30}
-					rows={10}
-					onChange={(ev) => setNewComment(ev.target.value)}
-				></textarea>
+		<ul className={css.existingComments}>
+			<span className={css.headerSession}>
+				<label>Comentários </label>
 
-				<button className={css.sendComment} onClick={registerComment}>
-					Enviar
-				</button>
-			</div>
-
-			<ul className={css.existingComments}>
-				<span className={css.headerSession}>
-					<label>Comentários </label>
-
-					<button
-						className={css.openSession}
-						onClick={() => setIsCollapsed((state) => !state)}
-					>
-						{isCollapsed ? <FiChevronUp /> : <FiChevronDown />}
-					</button>
-				</span>
-
-				<section
-					className={`${css.sessionComments} ${
-						isCollapsed ? css.expanded : css.collapsed
-					}`}
-					aria-expanded={isCollapsed}
+				<button
+					className={css.openSession}
+					onClick={() => setIsCollapsed((state) => !state)}
 				>
-					{comments?.length
-						? comments.map((data) => (
-								<li key={data.id}>
+					{isCollapsed ? <FiChevronUp /> : <FiChevronDown />}
+				</button>
+			</span>
+
+			<section
+				className={`${css.sessionComments} ${
+					isCollapsed ? css.expanded : css.collapsed
+				}`}
+				aria-expanded={isCollapsed}
+			>
+				{comments?.length
+					? comments.map((data) => (
+							<li key={data.id}>
+								<span className={css.header}>
 									<span className={css.author}>
 										<p>{data.username}</p>
-										<p>{data.createdAt.toLocaleString()}</p>
+										<p>{new Date(data.createdAt).toLocaleString()}</p>
 									</span>
 
-									<span className={css.comment}>
-										<p>{data.comment}</p>
-									</span>
-								</li>
-						  ))
-						: ""}
+									{user?.id === data.userId ? (
+										<button
+											title="Deletar comentário"
+											className={css.deleteComment}
+											onClick={() => {
+												const id = data.id;
+												commentMutate.mutate({ id });
+											}}
+										>
+											<AiOutlineCloseCircle className={css.iconError} />
+										</button>
+									) : (
+										""
+									)}
+								</span>
 
-					{!comments?.length ? (
-						<p className={css.noComment}>
-							Esse anime ainda não possui nenhum comentário! Seja o(a)
-							primeiro(a) a comentar!
-						</p>
-					) : (
-						""
-					)}
-				</section>
-			</ul>
-		</section>
+								<span className={css.comment}>
+									<p>{data.comment}</p>
+								</span>
+							</li>
+					  ))
+					: ""}
+
+				{!comments?.length ? (
+					<p className={css.noComment}>
+						Esse anime ainda não possui nenhum comentário! Seja o(a) primeiro(a)
+						a comentar!
+					</p>
+				) : (
+					""
+				)}
+			</section>
+		</ul>
 	);
 }
 
